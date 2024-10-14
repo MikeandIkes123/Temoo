815dc6824e00b72adc9902e27f8db7b68df8c4c2
derek <dh356@duke.edu>
2024-10-13 23:34:35 -0400

    added search purchases given uid

---
+++ app/myprofile.py
@@ -0,0 +1,17 @@
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_login import login_user, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+from .models.user import User
+
+
+from flask import Blueprint
+bp = Blueprint('myprofile', __name__)
+
+
+@bp.route('/myprofile')
+def myprofile():
+    return render_template('myprofile.html', title='My Profile')
